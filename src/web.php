f9361e985d1410a700d0167673419438e44e97d4
Arthur <artur.li@bk.ru>
2021-08-25 13:35:09 +0300

    add index

---
+++ src/web.php
@@ -0,0 +1,61 @@
+<?php
+
+
+use RetailCrm\Api\Interfaces\ClientExceptionInterface;
+use RetailCrm\Api\Factory\SimpleClientFactory;
+use RetailCrm\Api\Interfaces\ApiExceptionInterface;
+use RetailCrm\Api\Model\Entity\Orders\Delivery\OrderDeliveryAddress;
+use RetailCrm\Api\Model\Entity\Orders\Delivery\SerializedOrderDelivery;
+use RetailCrm\Api\Model\Entity\Orders\Items\Offer;
+use RetailCrm\Api\Model\Entity\Orders\Items\OrderProduct;
+use RetailCrm\Api\Model\Entity\Orders\Order;
+use RetailCrm\Api\Model\Entity\Orders\Payment;
+use RetailCrm\Api\Model\Request\Orders\OrdersCreateRequest;
+
+$apiUrl = 'https://demo.retailcrm.ru';
+$apiKey = 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb';
+
+$client = SimpleClientFactory::createClient($apiUrl, $apiKey);
+
+$request = new OrdersCreateRequest();
+$order = new Order();
+$payment = new Payment();
+$delivery = new SerializedOrderDelivery();
+$deliveryAddress = new OrderDeliveryAddress();
+$offer = new Offer();
+$item = new OrderProduct();
+
+
+$offer->name = 'Маникюрный набор AZ105R Azalita';
+$offer->article = 'AZ105R-Azalita';
+
+$item->offer = $offer;
+
+$order->items = [$item];
+$order->number = '29031996';
+$order->orderType = 'fizik';
+$order->orderMethod = 'test';
+$order->firstName = 'FirstName';
+$order->lastName = 'LastName';
+$order->patronymic = 'Patronymic';
+$order->customer['site'] = 'test';
+$order->status = 'trouble';
+$order->customerComment = 'https://github.com/Archi2903/rectailCrm-Api';
+$order->customFields = [
+    "prim" => 'https://docs.google.com/document/d/1coPXabBsKyVpKs9Qmf-P5rNpGoOw8Rf3k94YgiK7Xj8/edit',
+];
+
+$request->order = $order;
+$request->site = 'test';
+
+
+try {
+    $response = $client->orders->create($request);
+} catch (ApiExceptionInterface | ClientExceptionInterface $exception) {
+    echo $exception;
+    exit(-1);
+}
+
+echo "<pre>";
+var_dump($response->id);
+echo "</pre>";
